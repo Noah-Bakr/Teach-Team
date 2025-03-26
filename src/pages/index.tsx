625378d21b134a110ed328f7ae9daca11e77601f
Noah Bakr <s4095646@student.rmit.edu.au>
2025-03-26 19:52:05 +1100

    feat: email password input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
-import { Button, HStack, ButtonGroup, VStack } from "@chakra-ui/react";
+import { Button, AbsoluteCenter, Box, ButtonGroup, VStack, Input, Field, defineStyle } from "@chakra-ui/react";
 import { useState } from "react";
-
-
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <VStack>
-      <ButtonGroup size="md" colorPalette="yellow">
-        <Button variant="outline" onClick={() => setLoading(!loading)}>Cancel</Button>
-        <Button variant="solid" loading={loading} onClick={() => setLoading(!loading)}>Login</Button>
-      </ButtonGroup>
-    </VStack>
+    <AbsoluteCenter>
+      <Box position="relative" color={"white"} bg="white" p={4} borderRadius="md">
+        <VStack>
+          <VStack gap={4}>
+            <Input placeholder="Email" variant={"outline"} size="md" color={"black"} />
+            <Input placeholder="Password" type="password" variant={"outline"} size="md"/>
+          </VStack>
+          <ButtonGroup size="md" colorPalette="yellow">
+            <Button variant="outline" onClick={() => setLoading(!loading)}>Cancel</Button>
+            <Button variant="solid" loading={loading} onClick={() => setLoading(!loading)}>Login</Button>
+          </ButtonGroup>
+        </VStack>
+      </Box>
+    </AbsoluteCenter>
   );
 }
