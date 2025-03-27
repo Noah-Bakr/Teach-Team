d04b040b67c1e5749d5dcc91271f79cdae50f6b9
bodene-rmit <s3575564@student.rmit.edu.au>
2025-03-28 10:53:40 +1100

    feat: Footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>&copy; 2025 TeachTeam. All rights reserved.</p>
+                <p>Bodene Downie S3575564 & Noah Bakr S4095646</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import * as React from 'react'
+import * as React from 'react';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from '@/components/ui/provider';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <Provider>
         <Header />
-        <Component {...pageProps} />
+        <main className="main-content">
+            <Component {...pageProps} />
+        </main>
+        <Footer />
     </Provider>
     ); 
 }
