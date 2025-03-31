41c470ee81b2c72ba449eecd4c461f2a42be884e
Noah Bakr <s4095646@student.rmit.edu.au>
2025-03-31 22:04:09 +1100

    feat: login empty input validation, ui changes (field)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
-import { Button, AbsoluteCenter, Box, ButtonGroup, VStack, Input } from "@chakra-ui/react";
+import { Button, AbsoluteCenter, Box, ButtonGroup, VStack, Input, Field } from "@chakra-ui/react";
 import { useState } from "react";
+import { useRouter } from 'next/router';
+import { DEFAULT_USERS, User } from '@/types/user';
+import { PasswordInput } from "@/components/ui/password-input";
 
 export default function Home() {
+  const [users, setUsers] = useState<User[]>(DEFAULT_USERS);
   const [loading, setLoading] = useState(false);
+
+  const [error, setError] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmptyInput = () => {
+    if (email === '') {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+    if (password === '') {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
 
   return (
     /*
@@ -16,14 +42,20 @@ export default function Home() {
       <Box position="relative" color={"white"} bg="white" p={4} borderRadius="md">
         <VStack>
           <VStack gap={4}>
-            <Input placeholder="Email" variant={"outline"} size="md" color={"black"} focusRingColor={"yellow.500"}/>
-            <Input placeholder="Password" type="password" variant={"outline"} size="md"/>
-            
-            
+          <Field.Root color={"black"} invalid={emailError} required>
+            <Field.Label>Email <Field.RequiredIndicator /></Field.Label>
+            <Input placeholder="john@example.com" variant={"outline"} size="md" color={"black"} focusRingColor={"yellow.500"} value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <Field.ErrorText>This field is required</Field.ErrorText>
+          </Field.Root>
+          <Field.Root color={"black"} invalid={passwordError} required>
+            <Field.Label>Password <Field.RequiredIndicator /></Field.Label>
+            <PasswordInput placeholder="Password" variant={"outline"} size="md" color={"black"} focusRingColor={"yellow.500"} value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <Field.ErrorText>This field is required</Field.ErrorText>
+          </Field.Root>
           </VStack>
           <ButtonGroup size="md" colorPalette="yellow">
-            <Button variant="outline" onClick={() => setLoading(!loading)}>Cancel</Button>
-            <Button variant="solid" loading={loading} onClick={() => setLoading(!loading)}>Login</Button>
+            <Button variant="outline" onClick={() => { setLoading(false) }} >Cancel</Button>
+            <Button variant="solid" loading={loading} onClick={() => { handleEmptyInput() }} >Login</Button>
           </ButtonGroup>
         </VStack>
       </Box>
