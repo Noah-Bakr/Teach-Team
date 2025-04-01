583ae1448f6e0a23b00dacb804facb5edace854a
Noah Bakr <s4095646@student.rmit.edu.au>
2025-04-01 15:32:31 +1100

    fix: login advancment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,15 @@
-import { Button, AbsoluteCenter, Box, ButtonGroup, VStack, Input, Field, Alert } from "@chakra-ui/react";
+import { Button, AbsoluteCenter, Box, ButtonGroup, VStack, Input, Field, IconButton, Heading, Text, CloseButton, Presence, useDisclosure } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/router';
 import { DEFAULT_USERS, User } from '@/types/user';
 import { PasswordInput } from "@/components/ui/password-input";
 import { toaster } from "@/components/ui/toaster"
+import { LuX } from "react-icons/lu";
+import { on } from "events";
+import { useAuth } from "../context/AuthContext";
 
 export default function Home() {
   const [users, setUsers] = useState<User[]>(DEFAULT_USERS);
-  // Load saved users from localStorage
-  useEffect(() => {
-          const saved = localStorage.getItem('users');
-          if (saved) {
-            setUsers(JSON.parse(saved));
-          }
-      }, []);
-
-  // Save users state to localStorage when it changes.
-  useEffect(() => {
-          localStorage.setItem('users', JSON.stringify(users));
-      }, [users]);
   
   const [loading, setLoading] = useState(false);
 
@@ -30,8 +21,18 @@ export default function Home() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, currentUser } = useAuth();
+
+  const [loginVisibility, setLoginVisibility] = useState("");
+
+  // Load saved users from localStorage
+  useEffect(() => {
+    setLoginVisibility("open");
+    onToggle();
+  }, []);
 
   const handleLogin = () => {
+    // Check if email and password are empty. "return" prevents the rest of the function from executing
     if (email === '') {
       setEmailError(true);
       return;
@@ -41,62 +42,55 @@ export default function Home() {
       return;
     } else { setPasswordError(false); }
 
-    const user = users.find((user) => user.email === email && user.password === password);
-    if (user) {
-      setLoading(true);
-      setError('');
-      //Todo: save user to context
-      localStorage.setItem('currentUser', JSON.stringify(user)); // Save current user to localStorage (for session management)
+    setError("");
+
+    const success = login(email, password);
+    if (success) {
       //TODO: figure out how to make toaster stay to dashbaord
-      toaster.create({ title: "Login Successful", description: `Welcome back, ${user.firstName}!`, status: "success" });
-      router.push('/dashboard');
+      if (currentUser) {
+        toaster.create({ title: "Login Successful", description: `Welcome back, ${currentUser.firstName}!`, status: "success" });
+      }
+      router.push("/dashboard");
     } else {
-      setLoading(false);
-      setError('Invalid email or password');
+      setError("Invalid email or password");
     }
   };
 
+  const { open, onToggle } = useDisclosure()
+
   return (
-    /*
-    * every input has a form variable. on change update variable. onlick compare to localstorage
-    * if they are the same, set loading to true and redirect to /dashboard
-    * if they are not the same, set loading to false and show error message
-    * will turn this into a component later
-    * 
-    * <Alert.Root status="error">
-              <Alert.Indicator />
-              <Alert.Content>
-                <Alert.Title>Invalid Fields</Alert.Title>
-                <Alert.Description>
-                  Your form has some errors. Please fix them and try again.
-                </Alert.Description>
-              </Alert.Content>
-            </Alert.Root>
-    */
-
-    
-
-    <AbsoluteCenter>
-      <Box position="relative" color={"white"} bg="white" p={4} borderRadius="md">
-        <VStack>
-          <VStack gap={4}>
-            <Field.Root color={"black"} invalid={emailError} required>
-              <Field.Label>Email <Field.RequiredIndicator /></Field.Label>
-              <Input placeholder="john@example.com" variant={"outline"} size="md" color={"black"} focusRingColor={"yellow.500"} value={email} onChange={(e) => setEmail(e.target.value)}/>
-              <Field.ErrorText>This field is required</Field.ErrorText>
-            </Field.Root>
-            <Field.Root color={"black"} invalid={passwordError} required>
-              <Field.Label>Password <Field.RequiredIndicator /></Field.Label>
-              <PasswordInput placeholder="Password" variant={"outline"} size="md" color={"black"} focusRingColor={"yellow.500"} value={password} onChange={(e) => setPassword(e.target.value)}/>
-              <Field.ErrorText>This field is required</Field.ErrorText>
-            </Field.Root>
+    <Presence
+      present={open}
+      animationStyle={{ _open: "scale-fade-in", _closed: "scale-fade-out" }}
+      animationDuration="moderate">
+      <AbsoluteCenter>
+        <Box position="relative" color={"white"} bg="white" p={4} borderRadius={5} boxShadow="lg" minWidth="300px" width="auto" maxWidth="400px" height="auto" maxHeight="400px" >
+          <VStack alignItems={"center"} justifyContent={"center"} >
+            <CloseButton variant="ghost" colorPalette="yellow" position="absolute" top={2} right={2} fillOpacity={"0"} onClick={() => { onToggle(); }}/>
+            <Heading as="h1" color="black" textAlign={"center"}>Login</Heading>
+            <Text as="p" color="black" width={"300px"} textAlign={"center"}>Please enter your email and password to login.</Text>
+            <VStack gap={4}>
+              {/* <IconButton color="black" variant="ghost" position="absolute" top={2} right={2} onClick={() => { setLoginVisibility("closed") }} >
+                <LuX />
+              </IconButton> */}
+              <Field.Root width="300px" color={"black"} invalid={emailError} required>
+                <Field.Label>Email <Field.RequiredIndicator /></Field.Label>
+                <Input placeholder="john@example.com" variant={"outline"} size="md" color={"black"} focusRingColor={"yellow.500"} value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <Field.ErrorText>This field is required</Field.ErrorText>
+              </Field.Root>
+              <Field.Root color={"black"} invalid={passwordError} required>
+                <Field.Label>Password <Field.RequiredIndicator /></Field.Label>
+                <PasswordInput placeholder="Password" variant={"outline"} size="md" color={"black"} focusRingColor={"yellow.500"} value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <Field.ErrorText>This field is required</Field.ErrorText>
+              </Field.Root>
+              <ButtonGroup size="md" colorPalette="yellow">
+                <Button variant="outline" width="145" onClick={() => { setLoading(false); onToggle(); }} >Cancel</Button>
+                <Button variant="solid" width="145" loading={loading} onClick={() => { handleLogin(); }} >Login</Button>
+              </ButtonGroup>
+            </VStack>
           </VStack>
-          <ButtonGroup size="md" colorPalette="yellow">
-            <Button variant="outline" onClick={() => { setLoading(false) }} >Cancel</Button>
-            <Button variant="solid" loading={loading} onClick={() => { handleLogin() }} >Login</Button>
-          </ButtonGroup>
-        </VStack>
-      </Box>
-    </AbsoluteCenter>
+        </Box>
+      </AbsoluteCenter>
+    </Presence>
   );
 }
