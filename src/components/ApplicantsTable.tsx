661a9500ad063d0fe2f4fed0aee5559f06b3813e
bodene-rmit <s3575564@student.rmit.edu.au>
2025-04-02 17:52:33 +1100

    Added: Created Component for ApplicantsTable on Lecturers Page

---
+++ src/components/ApplicantsTable.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button, Table } from '@chakra-ui/react';
+import { Applicant } from '@/testData/types';
+
+interface ApplicantsTableProps {
+    applicants: Applicant[];
+    toggleSelect: (id: number) => void;
+}
+
+const ApplicantsTable: React.FC<ApplicantsTableProps> = ({ applicants, toggleSelect}) => (
+    <Table.Root size="sm">
+        <Table.Header>
+            <Table.Row>
+                <Table.ColumnHeader>Name</Table.ColumnHeader>
+                <Table.ColumnHeader>Course</Table.ColumnHeader>
+                <Table.ColumnHeader>Availability</Table.ColumnHeader>
+                <Table.ColumnHeader>Skills</Table.ColumnHeader>
+                <Table.ColumnHeader>Credentials</Table.ColumnHeader>
+                <Table.ColumnHeader>Action</Table.ColumnHeader>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            {applicants.map((applicant) => (
+                <Table.Row key={applicant.id}>
+                    <Table.Cell>{applicant.name}</Table.Cell>
+                    <Table.Cell>{applicant.course}</Table.Cell>
+                    <Table.Cell>{applicant.availability}</Table.Cell>
+                    <Table.Cell>{applicant.skills.join(", ")}</Table.Cell>
+                    <Table.Cell>{applicant.academicCredentials}</Table.Cell>
+                    <Table.Cell>
+                        <Button size="sm"
+                                colorScheme={applicant.selected ? 'red' : 'green'}
+                                onClick={() => toggleSelect(applicant.id)}>
+                            {applicant.selected ? 'Deselect' : 'Select'}
+                        </Button>
+                    </Table.Cell>
+                </Table.Row>
+            ))}
+        </Table.Body>
+    </Table.Root>
+);
+
+export default ApplicantsTable;
