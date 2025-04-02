762d4f6802d231892fbc6dc606c615650b381433
Noah Bakr <s4095646@student.rmit.edu.au>
2025-04-02 20:23:55 +1100

    Login link to button (not permanent)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import { Button } from "@chakra-ui/react";
 import "../styles/Navbar.css";
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="navbar">
-            <ul className="nav-list">
-                <li>
-                    <Link href="/sign-up">Sign Up</Link>
-                </li>
-                <li>
-                    <Link href="/sign-in">Sign In</Link>
-                </li>
-                <li>
-                    <Link href="/sign-out">Sign Out</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li>
+                        <Link href="/sign-up">Sign Up</Link>
+                    </li>
+                    <li>
+                        <Button>Log In</Button>
+                    </li>
+                    <li>
+                        <Link href="/sign-out">Sign Out</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        
     )
 }
 export default Navbar;
