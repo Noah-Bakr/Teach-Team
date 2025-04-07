a69be14e4b73d28d60c6a7d0f6b1ac4ab40ac767
Noah Bakr <s4095646@student.rmit.edu.au>
2025-04-07 10:56:32 +1000

    feat: password meter

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,7 +3,7 @@ import { Button, AbsoluteCenter, Box, ButtonGroup, VStack,
 import { useState, useEffect, use } from "react";
 import { useRouter } from 'next/router';
 import { DEFAULT_USERS, User } from '@/testData/user';
-import { PasswordInput } from "@/components/ui/password-input";
+import { PasswordInput, PasswordStrengthMeter } from "@/components/ui/password-input";
 import { toaster } from "@/components/ui/toaster"
 import { LuX, LuExternalLink } from "react-icons/lu";
 import { on } from "events";
@@ -23,6 +23,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ closeForm }) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordStrength, setPasswordStrength] = useState(0);
     const [emailError, setEmailError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
 
@@ -42,6 +43,44 @@ const LoginForm: React.FC<LoginFormProps> = ({ closeForm }) => {
     }
   }, [error]);
 
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const hasCorrectLength = password.length >= 8 && password.length <= 100;
+//   const hasNoCommonWords = !/(password|123456|qwerty|abc123|letmein|welcome|iloveyou)/i.test(password); //Will change to check to database
+//   const hasNoEmail = !email || !password.includes(email.split("@")[0]);
+//   const hasNoUsername = !password.includes(currentUser?.username || "");
+//   const hasNoFirstName = !password.includes(currentUser?.firstName || "");
+//   const hasNoLastName = !password.includes(currentUser?.lastName || "");
+
+  const validations = [
+    hasUpperCase, hasLowerCase, hasNumber, hasSpecialChar, hasCorrectLength 
+    //, hasNoCommonWords, hasNoEmail, hasNoUsername, hasNoFirstName, hasNoLastName
+  ];
+
+  const totalValidations = validations.length;
+
+
+  const incrementStrength = () => {
+    // let passwordStrength = 0;
+
+    // validations.forEach((validation) => {
+    //     if (validation) passwordStrength++;
+    // });
+
+    const passedValidations = validations.filter(validation => validation).length;
+
+    const strengthPercentage = (passedValidations / totalValidations) * 4;
+    setPasswordStrength(Math.round(strengthPercentage));
+    console.log("Password strength: ", strengthPercentage);
+    };
+
+  //Password validation
+  useEffect(() => {
+    incrementStrength();
+  }, [password]);
+
   const handleLogin = () => {
     // Check if email and password are empty. "return" prevents the rest of the function from executing
     if (email === '') {
@@ -55,7 +94,10 @@ const LoginForm: React.FC<LoginFormProps> = ({ closeForm }) => {
 
     setError("");
 
-    const success = login(email, password);
+    // const success = login(email, password); //uncomment in A2
+
+    //for A1, check email exists and password is strong
+    const success = users.some((user) => user.email === email && passwordStrength === 4);
     if (success) {
       if (currentUser) {
         toaster.create({ title: "Login Successful", description: `Welcome back, ${currentUser.firstName}!`, type: "success" });
@@ -92,6 +134,7 @@ const LoginForm: React.FC<LoginFormProps> = ({ closeForm }) => {
                             <PasswordInput className="LoginInput" placeholder="Password" variant={"outline"} value={password} onChange={(e) => setPassword(e.target.value)}/>
                             <Field.ErrorText>This field is required</Field.ErrorText>
                         </Field.Root>
+                        <PasswordStrengthMeter className="LoginInputFieldRoot" value={passwordStrength} />
                         <ButtonGroup className="LoginButtonGroup">
                             <Button className="LoginButton" colorPalette="yellow" variant="surface" onClick={() => { setLoading(false); onToggle(); closeForm(); }} >Cancel</Button>
                             <Button className="LoginButton" colorPalette="yellow" variant="solid" loading={loading} onClick={() => { handleLogin(); }} >Login</Button>
