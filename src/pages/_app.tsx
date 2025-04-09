016bb3f1047bd4231a2641ee97476051a84b5d7b
bodene-rmit <s3575564@student.rmit.edu.au>
2025-04-09 11:23:16 +1000

    Removed: Unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from '@/components/ui/provider';
-import { Toaster, toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster"
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { AuthProvider } from "../context/AuthContext";
