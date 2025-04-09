09c6d7f5c691e6fd8084332287928c97fee8e2ec
Noah Bakr <s4095646@student.rmit.edu.au>
2025-04-09 12:09:25 +1000

    feat: new login account system

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { Button } from "@chakra-ui/react";
+import { Avatar, Button, Menu, Portal } from "@chakra-ui/react";
 import "../styles/Navbar.css";
 import LoginForm from './LoginForm';
+import { useAuth } from '@/context/AuthContext';
 
 const Navbar: React.FC = () => {
+    const { currentUser, logout } = useAuth();
     const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
 
     const handleLoginClick = () => {
@@ -13,6 +15,10 @@ const Navbar: React.FC = () => {
 
     const closeLoginForm = () => {
         setIsLoginFormOpen(false);
+    };
+
+    const handleSignOutClick = () => {
+        logout();
     };
 
     return (
@@ -27,6 +33,26 @@ const Navbar: React.FC = () => {
                     </li>
                     <li>
                         <Link href="/sign-out">Sign Out</Link>
+                    </li>
+                    <li>
+                        <Menu.Root>
+                            <Menu.Trigger>
+                                <Avatar.Root colorPalette="yellow" size={"2xl"} >
+                                    <Avatar.Fallback />
+                                    <Avatar.Image src={currentUser?.avatar}/>
+                                </Avatar.Root>
+                            </Menu.Trigger>
+                            <Portal>
+                                <Menu.Positioner>
+                                    <Menu.Content>
+                                        {currentUser === null ? <><Menu.Item value="sign-in" onClick={handleLoginClick}>Sign In</Menu.Item>
+                                        <Menu.Item value="sign-up">Sign Up</Menu.Item></> : 
+                                        <><Menu.Item value="profile">Profile</Menu.Item>
+                                        <Menu.Item value="sign-out" color="fg.error"_hover={{ bg: "bg.error", color: "fg.error" }} onClick={handleSignOutClick}>Sign Out</Menu.Item></>}
+                                    </Menu.Content>
+                                </Menu.Positioner>
+                            </Portal>
+                        </Menu.Root>
                     </li>
                 </ul>
             </nav>
