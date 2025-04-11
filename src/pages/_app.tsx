a655b9aa863cb861ebf18197234a1fa86f23ddb9
bodene-rmit <s3575564@student.rmit.edu.au>
2025-04-11 16:41:28 +1000

    Updated: Add Container to created width and spacing across all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Container } from '@chakra-ui/react';
 import { Provider } from '@/components/ui/provider';
 import { Toaster } from "@/components/ui/toaster"
 import Header from '@/components/Header';
@@ -14,10 +15,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <DataInitialiser>
             <Provider>
                 <Header />
+                <Container maxW="1400px" py={2} px={2}>
                 <main className="main-content">
                     <Toaster />
                     <Component {...pageProps} />
                 </main>
+                </Container>
                 <Footer />
             </Provider>
         </DataInitialiser>
