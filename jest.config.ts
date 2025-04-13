1594e404f461c074348c30a4d8e56be2ecd9f97e
bodene-rmit <s3575564@student.rmit.edu.au>
2025-04-13 12:06:25 +1000

    Added: config settings for testing

---
+++ jest.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+    dir: './',
+});
+
+const config: Config = {
+    coverageProvider: 'v8',
+    testEnvironment: 'jest-environment-jsdom',
+
+    // structuredClone Issues
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+    moduleNameMapper: {
+        '^@/components/(.*)$': '<rootDir>/src/components/$1',
+        '^@/app/(.*)$': '<rootDir>/src/app/$1',
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    moduleDirectories: ['node_modules', '<rootDir>/'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/*.d.ts',
+        '!src/**/types.ts',
+    ],
+    watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname',
+    ],
+};
+
+
+export default createJestConfig(config);
