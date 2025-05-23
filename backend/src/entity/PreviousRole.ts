c81e76d704a08ce034f19d0e487dfcfb9695d0ef
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-23 16:47:06 +1000

    Added: PreviousRole.ts entity

--- backend/src/entity/PreviousRole.ts
+++ backend/src/entity/PreviousRole.ts
@@ -1,20 +1,50 @@
-import { Column } from "typeorm";
+// src/entities/PreviousRole.ts
+import { Entity, PrimaryGeneratedColumn, JoinColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './User';
 
+@Entity('PreviousRole')
 export class PreviousRole {
-    @Column({ type: "varchar", length: 254, nullable: true })
-    role: string;
+    @PrimaryGeneratedColumn()
+    previous_role_id: number;
 
-    @Column({ type: "varchar", length: 254, nullable: true })
+    @Column({ length: 255 })
+    previous_role: string;
+
+    @Column({ length: 255 })
     company: string;
 
-    // (new Date().toISOString())
-    @Column({ type: "varchar", length: 254, nullable: true })
-    startDate: string;
+    @Column('date')
+    start_date: Date;
 
-    // endDate can be null if the user is still employed
-    @Column({ type: "varchar", length: 254, nullable: true })
-    endDate: string | null;
+    @Column('date', { nullable: true })
+    end_date: Date;
 
-    @Column({ type: "varchar", length: 254, nullable: true })
+    @Column('text', { nullable: true })
     description: string;
+
+    // Many PreviousRoles can belong to one User (Many-to-One relationship)
+    @ManyToOne(() => User, user => user.previousRoles)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
 }
+
+// import { Column } from "typeorm";
+//
+// export class PreviousRole {
+//     @Column({ type: "varchar", length: 254, nullable: true })
+//     role: string;
+//
+//     @Column({ type: "varchar", length: 254, nullable: true })
+//     company: string;
+//
+//     // (new Date().toISOString())
+//     @Column({ type: "varchar", length: 254, nullable: true })
+//     startDate: string;
+//
+//     // endDate can be null if the user is still employed
+//     @Column({ type: "varchar", length: 254, nullable: true })
+//     endDate: string | null;
+//
+//     @Column({ type: "varchar", length: 254, nullable: true })
+//     description: string;
+// }
