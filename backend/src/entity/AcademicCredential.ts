81d95eb34e9052cd77b1186ee594e5bf0b633796
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-23 16:43:02 +1000

    Added: Academic Credential Entity

---
+++ backend/src/entity/AcademicCredential.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { AcademicCredentialUser } from './AcademicCredentialUser';
+
+@Entity('AcademicCredential')
+export class AcademicCredential {
+    @PrimaryGeneratedColumn()
+    academic_id: number;
+
+    @Column({ length: 100 })
+    degree_name: string;
+
+    @Column({ length: 100 })
+    institution: string;
+
+    @Column('date')
+    start_date: Date;
+
+    @Column('date', { nullable: true })
+    end_date: Date;
+
+    @Column('text', { nullable: true })
+    description: string;
+
+    // One academic credential can be associated with many users (One-to-Many relationship)
+    @OneToMany(() => AcademicCredentialUser, academicCredentialUser => academicCredentialUser.academicCredential)
+    academicCredentialUsers: AcademicCredentialUser[];
+}
