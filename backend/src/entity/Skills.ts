729954d0a3e2c39744228c5c8b9ab7ab9bda56a4
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-23 16:44:55 +1000

    Added: Skills.ts Entity

---
+++ backend/src/entity/Skills.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, JoinTable, ManyToMany } from 'typeorm';
+import { User } from './User';
+import { Course } from './Course';
+
+@Entity('Skills')
+export class Skills {
+    @PrimaryGeneratedColumn()
+    skill_id: number;
+
+    @Column({ length: 100 })
+    skill_name: string;
+
+    // Many users can have many skills (Many-to-Many relationship)
+    @ManyToMany(() => User, user => user.skills)
+    @JoinTable()
+    users: User[];
+
+    // Many courses can require many skills (Many-to-Many relationship)
+    @ManyToMany(() => Course, course => course.skills)
+    courses: Course[];
+}
