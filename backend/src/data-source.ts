87fa39641a43052f409c24fb751fea21865ead4c
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-25 15:31:56 +1000

    Updated: data-source to pull db info from env

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,23 +1,34 @@
 import 'dotenv/config';
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "./entity/User";
-import { Course } from "./entity/Course";
-import { Application } from "./entity/Application";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: 3306,
+    type: 'mysql',
+    host:     process.env.DB_HOST,
+    port:     3306,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     synchronize: true,
     logging: true,
-    entities: [User, Course, Application],
+    entities: [__dirname + '/entity/*.ts'],
     migrations: [],
     subscribers: [],
 });
+
+// export const AppDataSource = new DataSource({
+//     type: "mysql",
+//     host: process.env.DB_HOST,
+//     port: 3306,
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     synchronize: true,
+//     logging: true,
+//     entities: [User, Course, Application],
+//     migrations: [],
+//     subscribers: [],
+// });
 
   // synchronize: true will automatically create database tables based on entity definitions
   // and update them when entity definitions change. This is useful during development
