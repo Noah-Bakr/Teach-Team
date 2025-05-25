eba5c1302fef3726d915ac4ed22db4480d5a4747
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-25 15:33:08 +1000

    Added: index.ts to connect to db, tested connected and added the comments table to connect

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+import express, { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import commentRoutes from './routes/comment.routes';
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+
+// Test connection
+app.get("/ping", (_req: Request, res: Response) => {
+    res.send("pong");
+});
+
+// Routes
+app.use("/api", commentRoutes);
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) =>
+        console.log("Error during Data Source initialization:", error)
+    );
