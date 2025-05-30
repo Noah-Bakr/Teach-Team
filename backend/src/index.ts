e6e9f6a6cdee2565d25a695def9755674bdaa84b
Noah Bakr <noahbakr@outlook.com>
2025-05-30 20:46:25 +1000

    Updated: Configured CORS to allow credentials and set origin for API requests; added cookie-parser middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,14 @@ import commentRoutes from './routes/comment.routes';
 import authRoutes from './routes/auth.routes';
 import cors from "cors";
 import { seed } from "./seeds/seed";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Test connection
 app.get("/ping", (_req: Request, res: Response) => {
@@ -24,7 +26,7 @@ app.use("/api/auth", authRoutes);
 AppDataSource.initialize()
     .then(async () => {
         console.log("Data Source has been initialized!");
-        await seed();                // ← seed once
+        await seed();
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
