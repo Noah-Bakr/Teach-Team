3ad3b7db3c2f229d1e68a8122d1a4e6420a58559
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-31 17:03:05 +1000

    Updated: added application ranking table relationships

--- backend/src/entity/Application.ts
+++ backend/src/entity/Application.ts
@@ -2,6 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, JoinColum
 import { User } from './User';
 import { Course } from './Course';
 import { Comment} from "./Comment";
+import { ApplicationRanking} from "./ApplicationRanking";
 
 @Entity('Application')
 export class Application {
@@ -39,55 +40,7 @@ export class Application {
     // One application can have many comments (One-to-Many relationship)
     @OneToMany(() => Comment, comment => comment.application)
     comments: Comment[];
+
+    @OneToMany(() => ApplicationRanking, (ranking) => ranking.application)
+    rankings: ApplicationRanking[];
 }
-
-
-// import { Column, Entity, JoinTable, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-// import { User } from "./User";
-// import { Course } from "./Course";
-//
-// @Entity()
-// export class Application {
-//     @PrimaryGeneratedColumn({ type: "int" })
-//     application_id: number;
-//
-//     // Date of application (new Date().toISOString())
-//     @Column({ type: "varchar", length: 254 })
-//     date: string;
-//
-//     // Availability of the applicant
-//     @Column({ type: "varchar", length: 254 })
-//     availability: string[];
-//
-//     @Column({ type: "varchar", length: 254 })
-//     skills: string[]; // Skills of the applicant
-//
-//     // Academic credentials of the applicant
-//     @Column({ type: "varchar", length: 254 })
-//     academicCredentials: string | null;
-//
-//     // Previous roles of the applicant
-//     @Column({ type: "varchar", length: 254 })
-//     previousRoles: string[];
-//
-//     // Whether the application is selected or not
-//     @Column({ type: "boolean", default: false })
-//     selected: boolean;
-//
-//     // Rank of the application (optional)
-//     @Column({ type: "int", nullable: true })
-//     rank: number;
-//
-//     // Comments left by the lecturer (optional)
-//     @Column({ type: "varchar", length: 254, nullable: true })
-//     comment: string[];
-//
-//     // Many applications to one user
-//     @ManyToOne(() => User, (user) => user.applications)
-//     user: User;
-//
-//     // Many applications to one course
-//     @ManyToOne(() => Course, (course) => course.applications)
-//     @JoinTable()
-//     course: Course;
-// }
