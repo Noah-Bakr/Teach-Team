9e6bfb1a1920da92133bc8fd0c02c01cdbdfc323
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-31 17:01:38 +1000

    Updated: Previous role end date can be null

--- backend/src/entity/PreviousRole.ts
+++ backend/src/entity/PreviousRole.ts
@@ -17,7 +17,7 @@ export class PreviousRole {
     start_date: Date;
 
     @Column('date', { nullable: true })
-    end_date: Date;
+    end_date: Date | null;
 
     @Column('text', { nullable: true })
     description: string;
@@ -27,24 +27,3 @@ export class PreviousRole {
     @JoinColumn({ name: 'user_id' })
     user: User;
 }
-
-// import { Column } from "typeorm";
-//
-// export class PreviousRole {
-//     @Column({ type: "varchar", length: 254, nullable: true })
-//     role: string;
-//
-//     @Column({ type: "varchar", length: 254, nullable: true })
-//     company: string;
-//
-//     // (new Date().toISOString())
-//     @Column({ type: "varchar", length: 254, nullable: true })
-//     startDate: string;
-//
-//     // endDate can be null if the user is still employed
-//     @Column({ type: "varchar", length: 254, nullable: true })
-//     endDate: string | null;
-//
-//     @Column({ type: "varchar", length: 254, nullable: true })
-//     description: string;
-// }
