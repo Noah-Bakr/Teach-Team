e66d4fa732c188bb2942e47a80dac8821c8bfb17
bodene-rmit <s3575564@student.rmit.edu.au>
2025-05-31 17:02:40 +1000

    Updated: comments rooutes to follow pattern

--- backend/src/routes/comment.routes.ts
+++ backend/src/routes/comment.routes.ts
@@ -5,23 +5,23 @@ const router = Router();
 const commentController = new CommentController();
 
 router.get("/comments", async (req, res) => {
-    await commentController.all(req, res);
+    await commentController.getAllComments(req, res);
 });
 
 router.get("/comments/:id", async (req, res) => {
-    await commentController.one(req, res);
+    await commentController.getCommentById(req, res);
 });
 
 router.post("/comments", async (req, res) => {
-    await commentController.save(req, res);
+    await commentController.createComment(req, res);
 });
 
 router.put("/comments/:id", async (req, res) => {
-    await commentController.update(req, res);
+    await commentController.updateComment(req, res);
 });
 
 router.delete("/comments/:id", async (req, res) => {
-    await commentController.remove(req, res);
+    await commentController.deleteComment(req, res);
 });
 
 export default router;
