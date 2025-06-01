3fa772c615070d692c0773e70b4d0b4a9b18e119
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-01 15:19:47 +1000

    Updated: Many to many relationship management moved to TypeORM

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -31,4 +31,31 @@ router.delete("/user/:id", async (req, res) => {
     await userController.deleteUser(req, res);
 });
 
+// Attach / detach SKILLS:
+router.post('/user/:id/skills', async (req, res) => {
+    userController.addSkillsToUser(req, res);
+});
+
+router.delete('/user/:id/skills/:skillId', async (req, res) => {
+    userController.removeSkillFromUser(req, res);
+    });
+
+// Attach / detach ACADEMIC CREDENTIALS:
+router.post('/user/:id/academic-credentials', async (req, res) => {
+    userController.addCredentialsToUser(req, res);
+});
+
+router.delete('/user/:id/academic-credentials/:credentialId', async (req, res) => {
+    userController.removeCredentialFromUser(req, res);
+});
+
+// Attach / detach COURSES (lecturer):
+router.post('/user/:id/courses', async (req, res) => {
+    userController.addCoursesToUser(req, res)
+});
+
+router.delete('/user/:id/courses/:courseId', async (req, res) => {
+    userController.removeCourseFromUser(req, res);
+});
+
 export default router;
