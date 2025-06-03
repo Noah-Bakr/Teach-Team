917d2aa0dbbbb7e296ebc5eec09e0ce6bdd91682
Noah Bakr <s4095646@student.rmit.edu.au>
2025-06-03 16:06:10 +1000

    Enhance Navbar component to manage sign-up form state and update sign-up action

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,21 +3,31 @@ import { Avatar, Menu, Portal } from "@chakra-ui/react";
 import "../styles/Navbar.css";
 import LoginForm from './LoginForm';
 import { useAuth } from '@/context/AuthContext';
-import { toaster } from './ui/toaster';
 import { useRouter } from 'next/router';
+import SignUpForm from './SignUpForm';
 
 const Navbar: React.FC = () => {
     const { currentUser, logout } = useAuth();
-    const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
     const router = useRouter();
     
-
+    // State variables for login
+    const [isLoginFormOpen, setIsLoginFormOpen] = useState(false);
     const handleLoginClick = () => {
         setIsLoginFormOpen(true);
+        setIsSignUpFormOpen(false);
     };
-
     const closeLoginForm = () => {
         setIsLoginFormOpen(false);
+    };
+
+    // State variables for sign up
+    const [isSignUpFormOpen, setIsSignUpFormOpen] = useState(false);
+    const handleSignUpClick = () => {
+        setIsSignUpFormOpen(true);
+        setIsLoginFormOpen(false);
+    };
+    const closeSignUpForm = () => {
+        setIsSignUpFormOpen(false);
     };
 
     const handleSignOutClick = () => {
@@ -33,14 +43,14 @@ const Navbar: React.FC = () => {
                             <Menu.Trigger>
                                 <Avatar.Root colorPalette="yellow" size={"xl"} >
                                     <Avatar.Fallback />
-                                    <Avatar.Image src={currentUser?.avatar}/>
+                                    <Avatar.Image src={currentUser?.avatar ?? undefined}/>
                                 </Avatar.Root>
                             </Menu.Trigger>
                             <Portal>
                                 <Menu.Positioner>
                                     <Menu.Content>
                                         {currentUser === null ? <><Menu.Item value="sign-in" onClick={handleLoginClick}>Sign In</Menu.Item>
-                                        <Menu.Item value="sign-up" onClick={() => {toaster.create({ title: "Deployment Error", description: "The Sign Up form has not been deployed yet.", type: "info", duration: 6000 })}}>Sign Up</Menu.Item></> : 
+                                        <Menu.Item value="sign-up" onClick={handleSignUpClick}>Sign Up</Menu.Item></> : 
                                         <><Menu.Item value="dashboard" onClick={() => { router.push("/dashboard"); }}>Dashboard</Menu.Item>
                                         <Menu.Item value="profile" onClick={() => { router.push("/profile"); }}>Profile</Menu.Item>
                                         <Menu.Item value="sign-out" color="fg.error" _hover={{ bg: "bg.error", color: "fg.error" }} onClick={handleSignOutClick}>Sign Out</Menu.Item></>}
@@ -51,7 +61,8 @@ const Navbar: React.FC = () => {
                     </li>
                 </ul>
             </nav>
-            {isLoginFormOpen && <LoginForm closeForm={closeLoginForm} />}
+            {isLoginFormOpen && <LoginForm closeForm={closeLoginForm} openSignUpForm={handleSignUpClick}/>}
+            {isSignUpFormOpen && <SignUpForm closeForm={closeSignUpForm} />}
         </div>
         
     )
