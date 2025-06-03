e6fdc0d2fc2ea2ccbb2bc436edee7800ebab56db
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-04 08:38:39 +1000

    Removed: Comment routes

--- backend/src/routes/comment.routes.ts
+++ backend/src/routes/comment.routes.ts
@@ -1,29 +1,29 @@
-import { Router } from "express";
-import { CommentController } from "../controller/CommentController";
-import { validateDto } from "../middleware/validate";
-import { CreateCommentDto, UpdateCommentDto } from "../dto/comment.dto";
-
-const router = Router();
-const commentController = new CommentController();
-
-router.get("/", async (req, res) => {
-    await commentController.getAllComments(req, res);
-});
-
-router.get("/:id", async (req, res) => {
-    await commentController.getCommentById(req, res);
-});
-
-router.post("/", validateDto(CommentController), async (req, res) => {
-    await commentController.createComment(req, res);
-});
-
-router.put("/:id", validateDto(UpdateCommentDto), async (req, res) => {
-    await commentController.updateComment(req, res);
-});
-
-router.delete("/:id", async (req, res) => {
-    await commentController.deleteComment(req, res);
-});
-
-export default router;
+// import { Router } from "express";
+// import { CommentController } from "../controller/CommentController";
+// import { validateDto } from "../middleware/validate";
+// import { CreateCommentDto, UpdateCommentDto } from "../dto/comment.dto";
+//
+// const router = Router();
+// const commentController = new CommentController();
+//
+// router.get("/", async (req, res) => {
+//     await commentController.getAllComments(req, res);
+// });
+//
+// router.get("/:id", async (req, res) => {
+//     await commentController.getCommentById(req, res);
+// });
+//
+// router.post("/", validateDto(CommentController), async (req, res) => {
+//     await commentController.createComment(req, res);
+// });
+//
+// router.put("/:id", validateDto(UpdateCommentDto), async (req, res) => {
+//     await commentController.updateComment(req, res);
+// });
+//
+// router.delete("/:id", async (req, res) => {
+//     await commentController.deleteComment(req, res);
+// });
+//
+// export default router;
