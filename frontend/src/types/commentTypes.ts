270005dcf0d591bf8717b295225b0129d0e134fd
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-03 21:05:54 +1000

    Added: Comment Types

---
+++ frontend/src/types/commentTypes.ts
@@ -0,0 +1,7 @@
+export type CommentUI = {
+    id: number;             // comment_id
+    text: string;           // comment
+    createdAt: string;      // ISO datetime
+    updatedAt: string;      // ISO datetime
+    lecturerName: string;   // “First Last”
+};
