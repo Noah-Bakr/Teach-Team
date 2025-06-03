47e8b9ede5382e0bf8a85efac85822ce4dee0640
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-04 08:37:57 +1000

    Removed: Application Ranking routes

--- backend/src/routes/applicationRanking.routes.ts
+++ backend/src/routes/applicationRanking.routes.ts
@@ -1,29 +1,29 @@
-import { Router } from 'express';
-import { ApplicationRankingController } from '../controller/ApplicationRankingController';
-import { validateDto } from '../middleware/validate';
-import { CreateApplicationRankingDto, UpdateApplicationRankingDto } from '../dto/applicationRanking.dto';
-
-const router = Router();
-const applicationRankingController = new ApplicationRankingController();
-
-router.get("/", async (req, res) => {
-    await applicationRankingController.getAllApplicationRankings(req, res);
-});
-
-router.get("/:lecturer_id/:application_id", async (req, res) => {
-    await applicationRankingController.getApplicationRankingByIds(req, res);
-});
-
-router.post("/", validateDto(CreateApplicationRankingDto), async (req, res) => {
-    await applicationRankingController.createApplicationRanking(req, res);
-});
-
-router.put("/:lecturer_id/:application_id", validateDto(UpdateApplicationRankingDto), async (req, res) => {
-    await applicationRankingController.updateApplicationRanking(req, res);
-});
-
-router.delete("/:lecturer_id/:application_id", async (req, res) => {
-    await applicationRankingController.deleteApplicationRanking(req, res);
-});
-
-export default router;
+// import { Router } from 'express';
+// import { ApplicationRankingController } from '../controller/ApplicationRankingController';
+// import { validateDto } from '../middleware/validate';
+// import { CreateApplicationRankingDto, UpdateApplicationRankingDto } from '../dto/applicationRanking.dto';
+//
+// const router = Router();
+// const applicationRankingController = new ApplicationRankingController();
+//
+// router.get("/", async (req, res) => {
+//     await applicationRankingController.getAllApplicationRankings(req, res);
+// });
+//
+// router.get("/:lecturer_id/:application_id", async (req, res) => {
+//     await applicationRankingController.getApplicationRankingByIds(req, res);
+// });
+//
+// router.post("/", validateDto(CreateApplicationRankingDto), async (req, res) => {
+//     await applicationRankingController.createApplicationRanking(req, res);
+// });
+//
+// router.put("/:lecturer_id/:application_id", validateDto(UpdateApplicationRankingDto), async (req, res) => {
+//     await applicationRankingController.updateApplicationRanking(req, res);
+// });
+//
+// router.delete("/:lecturer_id/:application_id", async (req, res) => {
+//     await applicationRankingController.deleteApplicationRanking(req, res);
+// });
+//
+// export default router;
