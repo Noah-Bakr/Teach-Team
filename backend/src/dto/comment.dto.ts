c3be628ec6ff29206280a0fa94d5e10ef03bd665
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-04 08:38:28 +1000

    Removed: Comment dto

--- backend/src/dto/comment.dto.ts
+++ backend/src/dto/comment.dto.ts
@@ -1,23 +1,23 @@
-import {
-    IsInt,
-    IsString,
-    Min
-} from 'class-validator';
-
-export class CreateCommentDto {
-    @IsString()
-    comment: string;
-
-    @IsInt()
-    @Min(1)
-    application_id: number;
-
-    @IsInt()
-    @Min(1)
-    lecturer_id: number;
-}
-
-export class UpdateCommentDto {
-    @IsString()
-    comment?: string;
-}
+// import {
+//     IsInt,
+//     IsString,
+//     Min
+// } from 'class-validator';
+//
+// export class CreateCommentDto {
+//     @IsString()
+//     comment: string;
+//
+//     @IsInt()
+//     @Min(1)
+//     application_id: number;
+//
+//     @IsInt()
+//     @Min(1)
+//     lecturer_id: number;
+// }
+//
+// export class UpdateCommentDto {
+//     @IsString()
+//     comment?: string;
+// }
