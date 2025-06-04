1303c42d89da3b1616dbec557ea36c2d006340c1
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-04 20:23:12 +1000

    Updated: Comment cleanup

--- backend/src/entity/Course.ts
+++ backend/src/entity/Course.ts
@@ -31,23 +31,3 @@ export class Course {
     @ManyToMany(() => User, (user) => user.courses)
     lecturers: User[];
 }
-
-
-// import { Entity, Column, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-// import { Application } from "./Application";
-//
-// @Entity()
-// export class Course {
-//     @PrimaryGeneratedColumn({ type: "int" })
-//     user_id: number;
-//
-//     @Column({ type: "varchar", length: 40 })
-//     name: string;
-//
-//     @Column({ type: "varchar", length: 150, nullable: true })
-//     skills: string[];
-//
-//     // One course to many applications
-//     @OneToMany(() => Application, (application) => application.course)
-//     applications: Application[];
-// }
