bcf3cfa3ce29e68eecf5e095ded1abf3251b267b
Noah Bakr <s4095646@student.rmit.edu.au>
2025-06-04 18:08:03 +1000

    Add: implement user mapping function from backend to UI

---
+++ frontend/src/services/mappers/authMapper.ts
@@ -0,0 +1,28 @@
+import { UserUI } from "@/types/userTypes";
+import { User as BackendUser } from "@/services/api/userApi";
+
+export function mapBackendUserToUI(raw: BackendUser): UserUI {
+    return {
+        id: raw.user_id,
+        username: raw.username,
+        firstName: raw.first_name,
+        lastName: raw.last_name,
+        email: raw.email,
+        avatar: raw.avatar || null,
+        role: raw.role.role_name,
+        skills: raw.skills.map((s) => s.skill_name),
+        courses: raw.courses.map((c) => c.course_name),
+        previousRoles: raw.previousRoles.map((r) => r.previous_role),
+        academicCredentials: raw.academicCredentials.map((a) => a.degree_name),
+        reviews:
+            raw.reviews?.map((r) => ({
+            id: r.review_id,
+            rank: r.rank,
+            comment: r.comment,
+            reviewedAt: r.reviewed_at,
+            updatedAt: r.updated_at,
+            lecturerId: r.lecturer_id,
+            applicationId: r.application_id,
+            })) || undefined,
+    };
+}
