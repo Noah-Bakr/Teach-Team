4aabfc27a431616de49c450e3a70fe71880d60df
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-04 20:21:53 +1000

    Fixed: file routing changes

--- frontend/src/components/ApplicantsList.tsx
+++ frontend/src/components/ApplicantsList.tsx
@@ -1,10 +1,10 @@
 // src/components/ApplicationList.tsx
 import React, { useEffect, useState } from 'react';
-import { fetchAllApplications } from '../services/applicationApi';
-import { Application } from '../types/applicationTypes';
+import { fetchAllApplications } from '../services/applicationService';
+import { ApplicationUI } from '../types/applicationTypes';
 
 export function ApplicationList() {
-    const [applications, setApplications] = useState<Application[] | null>(null);
+    const [applications, setApplications] = useState<ApplicationUI[] | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
@@ -24,8 +24,8 @@ export function ApplicationList() {
             <h2>All Applications</h2>
             <ul>
                 {applications.map((app) => (
-                    <li key={app.application_id}>
-                        #{app.application_id} – {app.course.course_name} by {app.user.username} (
+                    <li key={app.id}>
+                        #{app.id} – {app.course.name} by {app.user.username} (
                         {app.status})
                     </li>
                 ))}
