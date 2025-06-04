10d7782bac801591acc7886210d56efc07807021
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-04 20:31:03 +1000

    Removed Ranking and comments completely from backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import express, { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
-// import commentRoutes from './routes/comment.routes';
 import authRoutes from './routes/auth.routes';
 import cors from "cors";
 import { seed } from "./seeds/seed";
@@ -11,7 +10,6 @@ import applicationRoutes from "./routes/application.routes";
 import previousRoleRoutes from "./routes/previousRole.routes";
 import roleRoutes from "./routes/role.routes";
 import skillsRoutes from "./routes/skills.routes";
-// import applicationRankingRoutes from "./routes/applicationRanking.routes";
 import reviewRoutes from "./routes/review.routes";
 import courseRoutes from "./routes/course.routes";
 import userRoutes from "./routes/user.routes";
@@ -20,7 +18,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3001;
 //const PORT = process.env.PORT || 3001;
 
-// (1) Log every request, to see exactly what path Express sees
+// Log every request, to see exactly what path Express sees
 app.use((req, res, next) => {
     console.log(`Incoming request → ${req.method} ${req.path}`);
     next();
@@ -30,18 +28,11 @@ app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Test connection
-app.get("/ping", (_req: Request, res: Response) => {
-    res.send("pong");
-});
-
 // Routes
 
 app.use("/api/academic-credentials", academicCredentialRoutes);
 app.use("/api/applications", applicationRoutes);
-// app.use("/api/application-rankings", applicationRankingRoutes);
 app.use("/api/auth", authRoutes);
-// app.use("/api/comments", commentRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/previous-roles", previousRoleRoutes);
 app.use("/api/reviews", reviewRoutes);
@@ -51,13 +42,12 @@ app.use("/api/users", userRoutes);
 
 AppDataSource.initialize()
     .then(async () => {
-        console.log("Data Source has been initialized!");
-        // By passing `"::"` as the second argument, Express will accept both IPv4 and IPv6.
+        console.log("Data Source has been initialised!");
         await seed();
         app.listen(PORT, "::", () => {
             console.log(`Server is running on port ${PORT}`);
         });
     })
     .catch((err) => {
-        console.error("Error during Data Source initialization:", err);
+        console.error("Error during Data Source initialisation:", err);
     });
