04ceae2713b7a4eb04ed756f69a3ff0a24d23a83
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-04 16:32:59 +1000

    Fixed: fixed to use common api

--- frontend/src/services/roleService.ts
+++ frontend/src/services/roleService.ts
@@ -1,24 +1,24 @@
-import axios from 'axios';
+import { api } from "./api";
 import { Role as BackendRole, CreateRoleDto, UpdateRoleDto } from './api/roleApi';
 import { mapRawRoleToUI } from './mappers/roleMapper';
 import { RoleUI } from '../types/roleTypes';
 
-const API_BASE = 'http://localhost:3001/api';
+//const API_BASE = 'http://localhost:3001/api';
 
 export async function fetchAllRoles(): Promise<RoleUI[]> {
-    const resp = await axios.get<BackendRole[]>(`${API_BASE}/roles`);
+    const resp = await api.get<BackendRole[]>(`/roles`);
     return resp.data.map(raw => mapRawRoleToUI(raw));
 }
 
 export async function fetchRoleById(id: number): Promise<RoleUI> {
-    const resp = await axios.get<BackendRole>(`${API_BASE}/roles/${id}`);
+    const resp = await api.get<BackendRole>(`/roles/${id}`);
     return mapRawRoleToUI(resp.data);
 }
 
 export async function createRole(
     payload: CreateRoleDto
 ): Promise<RoleUI> {
-    const resp = await axios.post<BackendRole>(`${API_BASE}/roles`, payload);
+    const resp = await api.post<BackendRole>(`/roles`, payload);
     return mapRawRoleToUI(resp.data);
 }
 
@@ -26,10 +26,10 @@ export async function updateRole(
     id: number,
     payload: UpdateRoleDto
 ): Promise<RoleUI> {
-    const resp = await axios.put<BackendRole>(`${API_BASE}/roles/${id}`, payload);
+    const resp = await api.put<BackendRole>(`/roles/${id}`, payload);
     return mapRawRoleToUI(resp.data);
 }
 
 export function deleteRole(id: number) {
-    return axios.delete<{ message: string }>(`${API_BASE}/roles/${id}`);
+    return api.delete<{ message: string }>(`/roles/${id}`);
 }
