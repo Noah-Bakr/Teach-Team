7013182f0e738d0747557ce0043646bf98376414
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-07 11:55:13 +1000

    Added: lecturer user access

---
+++ backend/src/routes/lecturer.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { LecturerController } from "../controller/LecturerController";
+import { isLecturer } from "../middleware/isLecturer";
+
+const router = Router();
+const lecturerController = new LecturerController();
+
+router.get("/:id/courses", isLecturer, async (req, res) => {
+    await lecturerController.getCourses(req, res);
+});
+
+router.get("/:id/applications", isLecturer, async (req, res) => {
+    await lecturerController.getApplications(req, res);
+});
+
+router.post("/applications/:id/review", isLecturer, async (req, res) => {
+    await lecturerController.saveReview(req, res);
+});
+
+router.get("/applications/:id/review", isLecturer, async (req, res) => {
+    await lecturerController.getReviewByApplication(req, res);
+});
+
+export default router;
