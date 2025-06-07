022cd958df8f8e13a7a81260fbceb1868b31a0f0
bodene-rmit <s3575564@student.rmit.edu.au>
2025-06-08 02:01:26 +1000

    Removed: data init

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Container } from '@chakra-ui/react';
 import { Provider } from '@/components/ui/provider';
@@ -7,12 +6,11 @@ import { Toaster } from "@/components/ui/toaster"
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { AuthProvider } from "@/context/AuthContext";
-import DataInitialiser from "@/components/DataInitialiser";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     return(
     <AuthProvider>
-        {/* <DataInitialiser> */}
             <Provider>
                 <Header />
                 <Container maxW="1400px" py={2} px={2}>
@@ -23,8 +21,6 @@ export default function App({ Component, pageProps }: AppProps) {
                 </Container>
                 <Footer />
             </Provider>
-        {/* </DataInitialiser> */}
     </AuthProvider>
-    
     ); 
 }
